eddb7d1593123c306954efa0e33c8dfa3f98a514
Sanma4 <sanmartinobenjamin@gmail.com>
2024-12-02 12:40:47 +0100

    New: Controlador Categorias

---
+++ BlogCore/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,32 @@
+﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriasController : Controller
+    {
+
+        private readonly IContedorTrabajo _contenedorTrabajo;
+
+        public CategoriasController(IContedorTrabajo contenedorTrabajo)
+        {
+            _contenedorTrabajo = contenedorTrabajo;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        #region Llamadas API
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new {data = _contenedorTrabajo.Categoria.GetAll()});
+        }
+        #endregion
+    }
+}
