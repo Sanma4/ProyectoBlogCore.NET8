85261a19df26dcdd6b6352d0d6307493f61567cd
Sanma4 <sanmartinobenjamin@gmail.com>
2024-12-04 12:55:51 +0100

    new: Vistas y controladores(Insert, edit & delete) de Categoria. SweetAlert y toast

--- BlogCore/Areas/Admin/Controllers/CategoriasController.cs
+++ BlogCore/Areas/Admin/Controllers/CategoriasController.cs
@@ -1,4 +1,5 @@
 ﻿using BlogCore.AccesoDatos.Data.Repository.IRepository;
+using BlogCore.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlogCore.Areas.Admin.Controllers
@@ -20,12 +21,71 @@ namespace BlogCore.Areas.Admin.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _contenedorTrabajo.Categoria.Add(categoria);
+                _contenedorTrabajo.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Categoria categoria = new Categoria();
+            categoria = _contenedorTrabajo.Categoria.Get(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                _contenedorTrabajo.Categoria.Update(categoria);
+                _contenedorTrabajo.Save();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(categoria);
+        }
 
         #region Llamadas API
         [HttpGet]
         public IActionResult GetAll()
         {
             return Json(new {data = _contenedorTrabajo.Categoria.GetAll()});
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _contenedorTrabajo.Categoria.Get(id);
+            if(objFromDb == null)
+            {
+                return Json(new {success = false, message = "Error intentando borrar la categoría"});
+            }
+
+            _contenedorTrabajo.Categoria.Remove(objFromDb);
+            _contenedorTrabajo.Save();
+            return Json(new { success = true, message = "Categoría borrada correctamente" });
         }
         #endregion
     }
--- BlogCore/wwwroot/js/categoria.js
+++ BlogCore/wwwroot/js/categoria.js
@@ -20,15 +20,15 @@ function cargarDataTable() {
             {
                 "data": "id",
                 "render": function (data) {
-                    return `<div class="text-center>"
-                                <a href="Admin/Categorias/Edit/${data}" class="btn btn-success text-white" style="cursor: pointer; width: 100px">;
-                                <i class="far fa-edit"> Editar</i>
+                    return `<div class="text-center">
+                                <a href="/Admin/Categorias/Edit/${data}" class="btn btn-success text-white" style="cursor:pointer; width:140px;">
+                                <i class="far fa-edit"></i> Editar
                                 </a>
                                 &nbsp;
-                                <a onclick=Delete("/Admin/Categorias/Delete/${data}") class="btn btn-danger text-white" style="cursor: pointer; width: 100px">;
-                                <i class="far fa-trash-alt"> Eliminar</i>
+                                <a onclick=Delete("/Admin/Categorias/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:140px;">
+                                <i class="far fa-trash-alt"></i> Borrar
                                 </a>
-                            </div>
+                          </div>
                     `;
                 }, "width": "30%"
             }
@@ -53,8 +53,33 @@ function cargarDataTable() {
                 "previous": "Anterior"
             }
         },
-        "width" : "100%"
+        "width": "100%"
 
-    })
+    });
+}
 
+function Delete(url) {
+    swal({
+        title: "¿Esta seguro de borrar?",
+        text: "Este contenido no se puede recuperar!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Si, borrar!",
+        closeOnconfirm: true
+    }, function () {
+        $.ajax({
+            type: 'DELETE',
+            url: url,
+            success: function (data) {
+                if (data.success) {
+                    toastr.success(data.message);
+                    dataTable.ajax.reload();
+                }
+                else {
+                    toastr.error(data.message);
+                }
+            }
+        });
+    });
 }
