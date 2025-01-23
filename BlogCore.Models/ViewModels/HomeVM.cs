dd5e9839396daeedd897c550c0368ec8e7db2ee6
Sanma4 <sanmartinobenjamin@gmail.com>
2025-01-23 12:25:42 +0100

    New: Agregadas cards, sliders y metodos en Home

---
+++ BlogCore.Models/ViewModels/HomeVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Slider> slider { get; set; }
+
+        public IEnumerable<Articulo> listArticulo { get; set; }
+    }
+}
--- BlogCore/Areas/Cliente/Controllers/HomeController.cs
+++ BlogCore/Areas/Cliente/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using BlogCore.AccesoDatos.Data.Repository.IRepository;
 using BlogCore.Models;
+using BlogCore.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,16 +9,25 @@ namespace BlogCore.Areas.Cliente.Controllers
     [Area("Cliente")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IContenedorTrabajo _contenedorTrabajo;
+
+        public HomeController(IContenedorTrabajo contenedorTrabajo)
         {
-            _logger = logger;
+            _contenedorTrabajo = contenedorTrabajo;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM()
+            {
+                slider = _contenedorTrabajo.Slider.GetAll(),
+                listArticulo = _contenedorTrabajo.Articulo.GetAll()
+            };
+
+            ViewBag.IsHome = true;
+
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
