7b369c1184a3fcc6b2293a7417ad5346e9fff7db
Sanma4 <sanmartinobenjamin@gmail.com>
2025-01-27 12:17:29 +0100

    New: Campos personalizados a la tabla AspUser

--- BlogCore.AccesoDatos/Data/ApplicationDbContext.cs
+++ BlogCore.AccesoDatos/Data/ApplicationDbContext.cs
@@ -14,7 +14,8 @@ namespace BlogCore.Data
         //Poner todos los modelos que se creen acá
         public DbSet<Categoria> Categoria { get; set; } 
         public DbSet<Articulo> Articulo { get; set; } 
-
-        public DbSet<Slider> Slider { get; set; }    
+        public DbSet<Slider> Slider { get; set; }  
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }  
+        
     }
 }
---
+++ BlogCore.Models/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required(ErrorMessage = "El nombre es requerido")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "La dirección es requerida")]
+        [Display(Name = "Dirección")]
+        public string Dirección { get; set; }
+
+        [Required(ErrorMessage = "La ciudad es requerida")]
+        public string Ciudad { get; set; }
+
+        [Required(ErrorMessage = "El país es requerido")]
+        [Display(Name = "País")]
+        public string Pais { get; set; }
+
+
+    }
+}
